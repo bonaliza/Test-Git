81793c777470a6f296c853fe726f4ba73feb7638
Admin <admin@Macintosh.local>
2014-10-04 10:33:24 +0700

    added a test class by bonaliza

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Admin on 10/4/2557 BE.
+//  Copyright (c) 2557 Bonaliza. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
